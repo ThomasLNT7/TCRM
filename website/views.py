844582d0d6c661309159ba1efbb4462328516262
Thomas Lenormant <lenormant.thomas@gmail.com>
2024-03-06 09:08:51 +0100

    logout / register page wip

--- website/views.py
+++ website/views.py
@@ -1,16 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    #check login
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
 
+        #authentification
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Vous êtes bien connecté !")
+            return redirect('home')
+        else:
+            messages.success(request, "Il y a eu une erreur, reconnectez vous :x !")
+            return redirect('home')
+    else:
+        return render(request, "home.html", {})
 
-def login_user(request):
-    pass
+def logout_user(request):
+    logout(request)
+    messages.success(request, "Vous vous êtes bien déconnecté")
+    return redirect('home')
 
-
-def log_out(request):
-    pass
+def register_user(request):
+    return render(request, "register.html", {})
